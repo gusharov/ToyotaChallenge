856f1a077a7ff2c97469f5a595b3fb5c2dd0532f
mustafahamoody <mustafa.a.hamoody@gmail.com>
2025-05-24 10:08:51 -0400

    initial commit

---
+++ solution.py
@@ -0,0 +1,79 @@
+from TMMC_Wrapper import *
+import rclpy
+import numpy as np
+import math
+import time
+from ultralytics import YOLO
+
+# Variable for controlling which level of the challenge to test -- set to 0 for pure keyboard control
+challengeLevel = 0
+
+# Set to True if you want to run the simulation, False if you want to run on the real robot
+is_SIM = True
+
+# Set to True if you want to run in debug mode with extra print statements, False otherwise
+Debug = False
+
+# Initialization    
+if not "robot" in globals():
+    robot = Robot(IS_SIM=is_SIM, DEBUG=Debug)
+    
+control = Control(robot)
+camera = Camera(robot)
+imu = IMU(robot)
+logging = Logging(robot)
+lidar = Lidar(robot)
+
+if challengeLevel <= 2:
+    control.start_keyboard_control()
+    rclpy.spin_once(robot, timeout_sec=0.1)
+
+
+try:
+    if challengeLevel == 0:
+        while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
+            # Challenge 0 is pure keyboard control, you do not need to change this it is just for your own testing
+
+    if challengeLevel == 1:
+        while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
+            # Write your solution here for challenge level 1
+            # It is recommended you use functions for aspects of the challenge that will be resused in later challenges
+            # For example, create a function that will detect if the robot is too close to a wall
+
+    if challengeLevel == 2:
+        while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
+            # Write your solution here for challenge level 2
+            
+    if challengeLevel == 3:
+        while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
+            # Write your solution here for challenge level 3 (or 3.5)
+
+    if challengeLevel == 4:
+        while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
+            # Write your solution here for challenge level 4
+
+    if challengeLevel == 5:
+        while rclpy.ok():
+            rclpy.spin_once(robot, timeout_sec=0.1)
+            time.sleep(0.1)
+            # Write your solution here for challenge level 5
+            
+
+except KeyboardInterrupt:
+    print("Keyboard interrupt received. Stopping...")
+
+finally:
+    control.stop_keyboard_control()
+    robot.destroy_node()
+    if rclpy.ok():
+        rclpy.shutdown()
